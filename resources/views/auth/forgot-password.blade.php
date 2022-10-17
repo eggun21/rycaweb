f440dc6e98e3561eaaedd6723cc6988aa260cba3
Raul Briceño <eggun21@gmail.com>
2022-10-16 20:40:26 -0400

    ajuste de idioma en español en pantallas de login, error de contraseña, registro, plantilla principal  app y guest

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Olvido su contraseña? No es problema. Nosotros enviaremos un correo a su direccion registrada para el cambio de contraseña.') }}
         </div>
 
         @if (session('status'))
@@ -20,13 +20,13 @@
             @csrf
 
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="{{ __('Corre Electronico') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enviar Correo con Enlace para Reiniciar Contraseña') }}
                 </x-jet-button>
             </div>
         </form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="shortcut icon" type="/img/ico.png" href="{{ asset('/img/ico4.png') }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
