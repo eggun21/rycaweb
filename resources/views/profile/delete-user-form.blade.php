41024e78932eb521895468a0c7ef7dde850360fb
Raul Briceño <eggun21@gmail.com>
2022-10-18 19:01:25 -0400

    ajuste de titulos en español, cambioa aespañol de validation error

--- resources/views/profile/delete-user-form.blade.php
+++ resources/views/profile/delete-user-form.blade.php
@@ -1,27 +1,27 @@
 <x-jet-action-section>
     <x-slot name="title">
-        {{ __('Delete Account') }}
+        {{ __('Borrar Cuenta') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Permanently delete your account.') }}
+        {{ __('Borrar tu cuenta permanentemente.') }}
     </x-slot>
 
     <x-slot name="content">
         <div class="max-w-xl text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Once your account is deleted, todos los recursos e informacion seran borrados permanentemente. Before deleting your account, please download any data or information that you wish to retain.') }}
         </div>
 
         <div class="mt-5">
             <x-jet-danger-button wire:click="confirmUserDeletion" wire:loading.attr="disabled">
-                {{ __('Delete Account') }}
+                {{ __('Borrar Cuenta') }}
             </x-jet-danger-button>
         </div>
 
         <!-- Delete User Confirmation Modal -->
         <x-jet-dialog-modal wire:model="confirmingUserDeletion">
             <x-slot name="title">
-                {{ __('Delete Account') }}
+                {{ __('Borrar Cuenta') }}
             </x-slot>
 
             <x-slot name="content">
