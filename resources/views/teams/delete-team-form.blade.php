a1133767d1a8d24dad82a9d0c618dfd56f254425
Raul Briceño <eggun21@gmail.com>
2022-10-19 14:31:04 -0400

    ajustando el idioma a español de archivos de grupos Team

--- resources/views/teams/delete-team-form.blade.php
+++ resources/views/teams/delete-team-form.blade.php
@@ -1,40 +1,40 @@
 <x-jet-action-section>
     <x-slot name="title">
-        {{ __('Delete Team') }}
+        {{ __('Borrar Grupo') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Permanently delete this team.') }}
+        {{ __('El grupo sera borrado permanentemente.') }}
     </x-slot>
 
     <x-slot name="content">
         <div class="max-w-xl text-sm text-gray-600">
-            {{ __('Once a team is deleted, all of its resources and data will be permanently deleted. Before deleting this team, please download any data or information regarding this team that you wish to retain.') }}
+            {{ __('de ser borrado un grupo se perdera irremediablemnte la informacion de dicho grupo. Antes de Borrar, por favor descargue la data e información registrada.') }}
         </div>
 
         <div class="mt-5">
             <x-jet-danger-button wire:click="$toggle('confirmingTeamDeletion')" wire:loading.attr="disabled">
-                {{ __('Delete Team') }}
+                {{ __('Borrado de Grupo') }}
             </x-jet-danger-button>
         </div>
 
         <!-- Delete Team Confirmation Modal -->
         <x-jet-confirmation-modal wire:model="confirmingTeamDeletion">
             <x-slot name="title">
-                {{ __('Delete Team') }}
+                {{ __('Borrar Grupo') }}
             </x-slot>
 
             <x-slot name="content">
-                {{ __('Are you sure you want to delete this team? Once a team is deleted, all of its resources and data will be permanently deleted.') }}
+                {{ __('Usted esta seguro de querer borrar este grupo? todos los recursos e información seran borrados irremediablemente.') }}
             </x-slot>
 
             <x-slot name="footer">
                 <x-jet-secondary-button wire:click="$toggle('confirmingTeamDeletion')" wire:loading.attr="disabled">
-                    {{ __('Cancel') }}
+                    {{ __('Cancelar') }}
                 </x-jet-secondary-button>
 
                 <x-jet-danger-button class="ml-3" wire:click="deleteTeam" wire:loading.attr="disabled">
-                    {{ __('Delete Team') }}
+                    {{ __('Borrar Grupo') }}
                 </x-jet-danger-button>
             </x-slot>
         </x-jet-confirmation-modal>
